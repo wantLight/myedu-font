a246b39a5301d81c32ce7a91a1d9fb66bcb12c22
xyzzg <1364932323@qq.com>
2019-09-04 21:45:07 +0800

    讲师列表更新

--- wsq-admin/src/views/edu/teacher/form.vue
+++ wsq-admin/src/views/edu/teacher/form.vue
@@ -1,53 +1,35 @@
 <template>
-  <div>
-    <div class="app-container">
-      <el-form label-width="120px">
-        <el-form-item label="讲师名称">
-          <el-input v-model="teacher.name"/>
-        </el-form-item>
-        <el-form-item label="讲师排序">
-          <el-input-number v-model="teacher.sort" :min="0" controls-position="right"/>
-        </el-form-item>
-        <el-form-item label="讲师头衔">
-          <el-select v-model="teacher.level" clearable placeholder="请选择">
-            <!--
+  <div class="app-container">
+    <el-form label-width="120px">
+      <el-form-item label="讲师名称">
+        <el-input v-model="teacher.name"/>
+      </el-form-item>
+      <el-form-item label="讲师排序">
+        <el-input-number v-model="teacher.sort" controls-position="right" :min="0"/>
+      </el-form-item>
+      <el-form-item label="讲师头衔">
+        <el-select v-model="teacher.level" clearable placeholder="请选择">
+          <!--
             数据类型一定要和取出的json中的一致，否则没法回填
             因此，这里value使用动态绑定的值，保证其数据类型是number
           -->
-            <el-option :value="1" label="高级讲师"/>
-            <el-option :value="2" label="首席讲师"/>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="讲师资历">
-          <el-input v-model="teacher.career"/>
-        </el-form-item>
-        <el-form-item label="讲师简介">
-          <el-input v-model="teacher.intro" :rows="10" type="textarea"/>
-        </el-form-item>
+          <el-option :value="1" label="高级讲师"/>
+          <el-option :value="2" label="首席讲师"/>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="讲师资历">
+        <el-input v-model="teacher.career"/>
+      </el-form-item>
+      <el-form-item label="讲师简介">
+        <el-input v-model="teacher.intro" :rows="10" type="textarea"/>
+      </el-form-item>
 
-        <!-- 讲师头像：TODO -->
-        <el-form-item label="头像上传">
-          <pan-thumb :image="teacher.avatar"/>
-          <el-button type="primary" icon="upload" @click="imagecropperShow=true">Change Avatar
-          </el-button>
+      <!-- 讲师头像：TODO -->
 
-          <image-cropper
-            v-show="imagecropperShow"
-            :width="300"
-            :height="300"
-            :key="imagecropperKey"
-            :url="BASE_API+'/admin/oss/file/upload'"
-            :max-size="2048"
-            field="file"
-            @close="close"
-            @crop-upload-success="cropSuccess"/>
-        </el-form-item>
-
-        <el-form-item>
-          <el-button :disabled="saveBtnDisabled" type="primary" @click="saveOrUpdate">保存</el-button>
-        </el-form-item>
-      </el-form>
-    </div>
+      <el-form-item>
+        <el-button :disabled="saveBtnDisabled" type="primary" @click="saveOrUpdate">保存</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
@@ -55,6 +37,15 @@
 import teacher from '@/api/edu/teacher'
 import ImageCropper from '@/components/ImageCropper'
 import PanThumb from '@/components/PanThumb'
+
+const defaultForm = {
+  name: '',
+  sort: 0,
+  level: '',
+  career: '',
+  intro: '',
+  avatar: ''
+}
 
 export default {
   components: { ImageCropper, PanThumb },
@@ -75,6 +66,14 @@ export default {
     }
   },
 
+  watch: {
+    $route(to, from) {
+      console.log('watch $route')
+      this.init()
+    }
+  },
+
+  // 页面渲染前调用fetchDataById
   created() {
     console.log('created')
     // 判断路由参数
@@ -82,9 +81,21 @@ export default {
       const id = this.$route.params.id
       this.fetchDataById(id)
     }
+    this.init()
   },
 
   methods: {
+
+    init() {
+      if (this.$route.params && this.$route.params.id) {
+        const id = this.$route.params.id
+        this.fetchDataById(id)
+      } else {
+        // 使用对象拓展运算符，拷贝对象，而不是引用，
+        // 否则新增一条记录后，defaultForm就变成了之前新增的teacher的值
+        this.teacher = { ...defaultForm }
+      }
+    },
 
     saveOrUpdate() {
       this.saveBtnDisabled = true
--- wsq-admin/src/views/edu/teacher/list.vue
+++ wsq-admin/src/views/edu/teacher/list.vue
@@ -1,6 +1,41 @@
 <template>
   <div class="app-container">
 
+    <!--查询表单-->
+    <el-form :inline="true" class="demo-form-inline">
+      <el-form-item>
+        <el-input v-model="searchObj.name" placeholder="讲师名"/>
+      </el-form-item>
+
+      <el-form-item>
+        <el-select v-model="searchObj.level" clearable placeholder="讲师头衔">
+          <el-option :value="1" label="高级讲师"/>
+          <el-option :value="2" label="首席讲师"/>
+        </el-select>
+      </el-form-item>
+
+      <el-form-item label="添加时间">
+        <el-date-picker
+          v-model="searchObj.begin"
+          type="datetime"
+          placeholder="选择开始时间"
+          value-format="yyyy-MM-dd HH:mm:ss"
+          default-time="00:00:00"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-date-picker
+          v-model="searchObj.end"
+          type="datetime"
+          placeholder="选择截止时间"
+          value-format="yyyy-MM-dd HH:mm:ss"
+          default-time="00:00:00"
+        />
+      </el-form-item>
+
+      <el-button type="primary" icon="el-icon-search" @click="fetchData()">查询</el-button>
+      <el-button type="default" @click="resetData()">清空</el-button>
+    </el-form>
     <el-table v-loading = "listLoading" :data="list" element-loading-text="数据加载中" border fit highlight-current-row>
       <el-table-column label="序号" width="70" align="center">
         <template slot-scope="scope">
@@ -98,6 +133,11 @@ export default {
           })
         }
       })
+    },
+
+    resetData() {
+      this.searchObj = {}
+      this.fetchData(1)
     }
 
   }
